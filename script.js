aba26134423a20d840fbac2c6642f4ee8ba35356
buttasam <samuel.butta@seznam.cz>
2015-09-22 02:31:06 +0200

    translated to english

--- script.js
+++ script.js
@@ -1,65 +1,53 @@
 $(function() {
-	
-	var canvas = document.getElementById("platno");
+
+	var canvas = document.getElementById("board");
 	var g = canvas.getContext("2d");
-	
+
 	var img = new Image();
 	img.onload = function () {
 	    g.drawImage(img, 0, 0);
 	}
 	img.src = "img/space.png";
 
-	// herní smyčka
+	// game loop
 	setInterval(function() {
 		gameloop(g);
 	}, 30);
-	
-	
-	$("#platno").mousedown(function(e) {
-		    
-		    var mouseX, mouseY;
 
-	        mouseX = e.offsetX;
-	        mouseY = e.offsetY;
-
-	        $("#xSpan").html(mouseX);
-	        $("#ySpan").html(mouseY);
-	});
-	
 });
 
-var Planeta = function(x, y, rychlost, polomer, vzdalenost) {
+var Planet = function(x, y, speed, radius, distance) {
 	this.x = x;
 	this.y = y;
-	this.rychlost = rychlost;
-	this.polomer = polomer;
-	this.vzdalenost = vzdalenost;
-	this.uhel = randUhel();
-	
+	this.speed = speed;
+	this.radius = radius;
+	this.distance = distance;
+	this.angle = randAngle();
+
 	this.update = function()
 	{
-		if(this.uhel >= Math.PI * 2)
+		if(this.angle >= Math.PI * 2)
 		{
-		this.uhel = 0;
+			this.angle = 0;
 		}
-		this.uhel += ((Math.PI * 2) / 100) / this.rychlost;		
-		
-		this.x = Math.sin(this.uhel) * this.vzdalenost + slunce.x;
-		this.y = Math.cos(this.uhel) * this.vzdalenost + slunce.y;
+		this.angle += ((Math.PI * 2) / 100) / this.speed;
+
+		this.x = Math.sin(this.angle) * this.distance + sun.x;
+		this.y = Math.cos(this.angle) * this.distance + sun.y;
 	}
 }
 
-var slunce = new Planeta(400, 400, 4, 40, 0);
-var merkur = new Planeta(0, 0, 0.240, 5, 60);
-var venuse = new Planeta(0, 0, 0.615, 7, 90);
-var zeme = new Planeta(0, 0, 1, 9, 130);
-var mars = new Planeta(0, 0, 1.8808, 8, 180);
-var jupiter = new Planeta(0, 0, 11.87, 15, 230);
-var saturn = new Planeta(0, 0, 29.45, 13, 290);
-var uran = new Planeta(0, 0, 84.07, 12, 350);
-var neptun = new Planeta(0, 0, 164.79, 11, 385);
+var sun = new Planet(400, 400, 4, 40, 0);
+var mercur = new Planet(0, 0, 0.240, 5, 60);
+var venus = new Planet(0, 0, 0.615, 7, 90);
+var earth = new Planet(0, 0, 1, 9, 130);
+var mars = new Planet(0, 0, 1.8808, 8, 180);
+var jupiter = new Planet(0, 0, 11.87, 15, 230);
+var saturn = new Planet(0, 0, 29.45, 13, 290);
+var uran = new Planet(0, 0, 84.07, 12, 350);
+var neptun = new Planet(0, 0, 164.79, 11, 385);
 
-var pocetObehu = 0;
+var countOfLoops = 0;
 
 function gameloop(g)
 {
@@ -68,49 +56,49 @@ function gameloop(g)
 
 function render(g)
 {
-	//vyčištění plátna
-    clear(g);
+	//clear board
+	clear(g);
 
-	kruh(g, slunce.x, slunce.y, slunce.polomer, "yellow");	
-	
-	merkur.update();
-	kruh(g, merkur.x, merkur.y, merkur.polomer, "grey");
-	
-	venuse.update();
-	kruh(g, venuse.x, venuse.y, venuse.polomer, "rgba(255, 152, 61 ,1)");
-	
-	zeme.update();
-	kruh(g, zeme.x, zeme.y, zeme.polomer, "blue");	
-	
+	circle(g, sun.x, sun.y, sun.radius, "yellow");
+
+	mercur.update();
+	circle(g, mercur.x, mercur.y, mercur.radius, "grey");
+
+	venus.update();
+	circle(g, venus.x, venus.y, venus.radius, "rgba(255, 152, 61 ,1)");
+
+	earth.update();
+	circle(g, earth.x, earth.y, earth.radius, "blue");
+
 	mars.update();
-	kruh(g, mars.x, mars.y, mars.polomer, "red");	
-	
-	jupiter.update();
-	kruh(g, jupiter.x, jupiter.y, jupiter.polomer,  "rgba(255, 211, 61 ,1)");
-	
-	saturn.update();
-	kruh(g, saturn.x, saturn.y, saturn.polomer,  "rgba(245, 238, 144 ,1)");
-	
-	uran.update();
-	kruh(g, uran.x, uran.y, uran.polomer,  "rgba(137, 228, 246 ,1)");
-	
-	neptun.update();
-	kruh(g, neptun.x, neptun.y, neptun.polomer,  "rgba(137, 151, 246 ,1)");
+	circle(g, mars.x, mars.y, mars.radius, "red");
 
-    g.font = "26px Arial";
-    g.textAlign = "left";
-    g.fillStyle = "white";
-    g.fillText("Model sluneční soustavy", 50, 50);
-    	
+	jupiter.update();
+	circle(g, jupiter.x, jupiter.y, jupiter.radius,  "rgba(255, 211, 61 ,1)");
+
+	saturn.update();
+	circle(g, saturn.x, saturn.y, saturn.radius,  "rgba(245, 238, 144 ,1)");
+
+	uran.update();
+	circle(g, uran.x, uran.y, uran.radius,  "rgba(137, 228, 246 ,1)");
+
+	neptun.update();
+	circle(g, neptun.x, neptun.y, neptun.radius,  "rgba(137, 151, 246 ,1)");
+
+	g.font = "26px Arial";
+	g.textAlign = "left";
+	g.fillStyle = "white";
+	g.fillText("Model our the SOLAR SYSTEM", 50, 50);
+
 }
 
-function kruh(g, x, y, radius, color)
+function circle(g, x, y, radius, color)
 {
 	g.beginPath();
 	g.fillStyle = color;
 	g.arc(x, y, radius, 0, Math.PI * 2);
 	g.fill();
-	g.closePath();	
+	g.closePath();
 }
 
 function clear(g)
@@ -123,7 +111,7 @@ function randNum(from, to)
 	return (Math.floor(Math.random() * to) + from);
 }
 
-function randUhel()
+function randAngle()
 {
 	return (Math.PI * (Math.random() + randNum(0, 2)));
 }
